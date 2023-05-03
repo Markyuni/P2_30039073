1fe9594363f9ab45c00374f1786be215210eb0b5
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-05-03 09:52:34 -0400

    Adiciones de la primera asignacion

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req,res) => {
+	res.send({
+		message: "Hola Mundo | Estudiante: Jesús Ramírez | C.I.: 30.039.073 | Sección: '1'"
+	})
+});
+
+app.listen(port, () => {
+	console.log("Escuchando al puerto 4000 en http::/localhost:" + 4000);
+});
+
