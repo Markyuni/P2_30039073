0ed03e3b87f807f4149565030477a8b0962b6ed6
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-05-03 20:26:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/user');
+
 const app = express();
-const port = 4000;
 
-app.get('/', (req,res) => {
-	res.send({
-		message: "Hola Mundo | Estudiante: Jesús Ramírez | C.I.: 30.039.073 | Sección: '1'"
-	})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/contacto', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-	console.log("Escuchando al puerto 4000 en http::/localhost:" + 4000);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
+module.exports = app;
