3f332ac1795ed06cb72305e140b5d563e7bc317d
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-05-03 20:08:17 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Datos del Estudiante',
+    nombre: 'Jesús', apellido: 'Ramírez',
+    cedula: '30.039.073',
+    seccion: '1'
+  });
 });
 
 module.exports = router;
