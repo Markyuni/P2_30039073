f23224c06b050ce00a85ffaa10bc9bec88f29949
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-05-30 09:54:43 -0400

    Adición de Nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const db = require('../database');
 const axios = require('axios');
+const nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
@@ -22,9 +23,9 @@ router.post('/', function(req, res, next) {
 
   const myIP = ip.split(",")[0];
 
-  // axios.get(`http://ip-api.com/json/186.92.134.244?fields=country`).then((res) => {
+  axios.get(`http://ip-api.com/json/186.92.134.244?fields=country`).then((res) => { /* local */
 
-  axios.get(`http://ip-api.com/json/${myIP}`).then((res) => {
+  // axios.get(`http://ip-api.com/json/${myIP}`).then((res) => {                    /* render */
     const pais = res.data.country;
 
     console.log({ name, email, comment, date, myIP, pais });
@@ -34,6 +35,30 @@ router.post('/', function(req, res, next) {
     console.log(error)
   })
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.hostinger.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'test009@arodu.dev',
+    pass: 'eMail.test009'
+  }
+});
+
+const mailOptions = {
+  from: 'test009@arodu.dev',
+  to: 'jesusorlandoramirezsierra@yahoo.com.ve',
+  subject: 'Envío de datos',
+  text: 'Datos de formulario: ' + 'Nombre: ' name ', ' + 'Correo: ' email ', ' + 'Comentario: ' comment ', ' + 'Fecha: ' date ', ' + 'IP: ' myIP ', ' + 'Pais: ' pais '.'
+}
+
+transporter.sendMail(mailOptions, function(error,info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Correo enviado: ' + info.response);
+  }
+});
 
   res.redirect('/');
 });
