f10362eb7a99f61683ce5755cf2fc1c7e47eaee8
Markyuni <132387560+Markyuni@users.noreply.github.com>
2023-05-31 15:31:35 -0400

    Posibles últimas modificaciones

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	HOST: process.env.HOST || 'smtp.hostinger.com',
 	AUTH_USER_FROM: process.env.AUTH_USER_FROM || 'test009@arodu.dev',
 	AUTH_PASS: process.env.AUTH_PASS || 'eMail.test009',
-	TO: process.env.TO || 'jesusorlandoramirezsierra@yahoo.com.ve',
+	TO: process.env.TO || '`programacion2ais@dispostable.com',
 	SUBJECT: process.env.SUBJECT || 'Envío de datos',
 	// Llave privada de reCAPTCHA
 	PRIVATE_KEY: process.env.PRIVATE_KEY || '6LeMHU0mAAAAAK5dDvya0gcFwBrP6olRxkQzjzr0'
